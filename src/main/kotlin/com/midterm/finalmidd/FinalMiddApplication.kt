2a900d277f1d1dd1ca87505fb1370a6ac52d3120
ulkiller <ulkersirinov@gmail.com>
2024-10-25 14:58:09 +0400

    salam

---
+++ src/main/kotlin/com/midterm/finalmidd/FinalMiddApplication.kt
@@ -0,0 +1,11 @@
+package com.midterm.finalmidd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinalMiddApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinalMiddApplication>(*args)
+}
---
+++ src/test/kotlin/com/midterm/finalmidd/FinalMiddApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.midterm.finalmidd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinalMiddApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
